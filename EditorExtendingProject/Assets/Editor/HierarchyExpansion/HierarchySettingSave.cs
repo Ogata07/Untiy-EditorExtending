40ede2216cc515bf98499e4b9d56a4b2f6f5c8d4
Ogata07 <ogatayuki07@gmail.com>
2024-01-29 00:24:57 +0900

    設定画面の作成
    設定を保存するスクリプタブルオブジェクトの作成途中

---
+++ EditorExtendingProject/Assets/Editor/HierarchyExpansion/HierarchySettingSave.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// hierarchy表示改造の設定を保存する
+/// </summary>
+[CreateAssetMenu(menuName ="Test")]
+public class HierarchySettingSave : ScriptableObject
+{
+    public bool TagSwicth;
+    public bool LayerSwicth;
+}
